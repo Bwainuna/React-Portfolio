b9bc27baa361aa0c4f79a34eb179b188ac08fd62
Bwainuna <blakebah2@gmail.com>
2023-11-30 13:35:38 -0700

    changed footer path on line 5 of app.jsx due to case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
